511b04bed27d5737103fdadf9cabe476f56db091
Quanhu Sheng <shengqh@gmail.com>
2016-12-02 10:04:12 -0600

    v1.6.18

---
+++ Genome/Mapping/TestChromosomeCountProcessor.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQS.Genome.Mapping
+{
+  [TestFixture]
+  public class TestChromosomeCountProcessor
+  {
+    [Test]
+    public void Test()
+    {
+      var processor = new ChromosomeCountProcessor(new ChromosomeCountProcessorOptions()
+      {
+        InputFile = "../../data/TestChromosomeCountProcessor.sam",
+        CountFile = "../../data/TestChromosomeCountProcessor.dupcount",
+        OutputFile = "../../data/TestChromosomeCountProcessor.sam.count",
+        KeepSequence=true
+      });
+      processor.Process();
+    }
+  }
+}
